d73da32e0c86a002eb36de15a7b27bfa5a2e23c0
Jboy Flaga <jeremiahflaga@gmail.com>
2014-06-11 16:31:58 +0800

    Initial Commit

---
+++ StudentInfo.Specs/Controllers/HomeController/when_the_home_controller_is_told_to_display_the_default_view.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+using StudentInfo.WebUI.Controllers;
+using Microsoft.AspNet.Mvc;
+
+namespace StudentInfo.Specs
+{
+    /// <summary>
+    /// Summary description for when_the_home_controller_is_told_to_display_the_default_view
+    /// </summary>
+    /// 
+    [Trait("Home Controller", "")]
+    public class when_the_home_controller_is_told_to_display_the_default_view
+    {
+        HomeController _controller;
+        ViewResult _viewResult;
+
+        public when_the_home_controller_is_told_to_display_the_default_view()
+        {
+            // given
+            _controller = new HomeController();
+
+            // when
+            _viewResult = _controller.Index() as ViewResult;
+        }
+
+        [Fact]
+	    public void it_should_return_the_home_view()
+	    {
+            // then
+            Assert.True(string.IsNullOrEmpty(_viewResult.ViewName));
+	    }
+
+        [Fact]
+        public void it_should_display_the_welcome_message_in_the_view()
+        {
+            // then
+            Assert.Equal("Welcome!", _controller.ViewBag.Message);
+        }
+    }
+}
---
+++ StudentInfo.Specs/SampleTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Specs
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void True_is_true()
+        {
+            Assert.True(true);
+        }
+    }
+}
